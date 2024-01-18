b5da1abb3030c71a1d2533af0530fe8e2a9ceaa5
Ramsha <fnuramsha@gmail.com>
2024-01-18 19:47:46 +0100

    added random generator function

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 'use strict';
 
+let nestedObject = {
+    firstArray : ['know', 'understand', 'realize', 'recognize'],
+    secondArray : ['greater', 'significant','stronger', 'powerful']
+ }
+
 const message = document.querySelector('.message');
 
+function randomGenerator() {
+    const arrayIndex1 = Math.floor(Math.random() * nestedObject.firstArray.length)
+    const arrayIndex2 = Math.floor(Math.random() * nestedObject.secondArray.length)
+    const arrayVal1 = nestedObject.firstArray[arrayIndex1]
+    const arrayVal2 = nestedObject.secondArray[arrayIndex2]
+    return alert(`Believe in yourself and all that you are ${arrayVal1} that there is something inside you that is ${arrayVal2} than any obstacle'
+    `)
+}
+
 message.addEventListener('click', function() {
-    const array = ['Believe you can, and you are halfway there', 
-    'You are stronger than you think', 
-    'Stay positive, work hard, make it happen',
-    'Embrace the journey, not just the destination', 
-    'Dream big, work hard', 'Make today amazing'
-]
-
-    const arrayIndex = Math.floor(Math.random() * array.length)
-    const arrayVal = array[arrayIndex]
-    alert(arrayVal)
-
+    randomGenerator()
 })
+
+
+
+
 
